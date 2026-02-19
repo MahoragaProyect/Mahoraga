37c0d4f8b152b544578f966137c585695d5ca726
lmejiacoronado-arch <lmejiacoronado@gmail.com>
2026-02-18 20:51:14 -0500

    dashboard con python

---
+++ js/data/maps/pythonMap.js
@@ -0,0 +1,15 @@
+export const pythonMap = [
+
+    { id: 1, title: "Variables", x: 100, y: 250, requires: null },
+
+    { id: 2, title: "Tipos", x: 350, y: 150, requires: 1 },
+
+    { id: 3, title: "Condicionales", x: 600, y: 300, requires: 2 },
+
+    { id: 4, title: "Bucles", x: 900, y: 200, requires: 3 },
+
+    { id: 5, title: "Funciones", x: 1200, y: 300, requires: 3 },
+
+    { id: 6, title: "POO", x: 1500, y: 200, requires: 4 },
+
+];
