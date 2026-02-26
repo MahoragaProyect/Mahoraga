2a1c38f795be82fe7acb032cb9005f2cc86bc5a2
lopezzuluagaj3-colab <lopezzuluagaj3@gmail.com>
2026-02-25 21:09:43 -0500

    se modularizo la conexion a la base de datos, se crearon carpetas config, contralles, routes, server y app, se crea la opcion de consultar la preguntas y crearlas las preguntas

--- src/app.js
+++ src/app.js
@@ -1,26 +1,9 @@
-import { Client } from "pg"
-import dotenv from "dotenv"
-dotenv.config()
+import express from 'express'
+import questionRoutes from './routes/question.route.js'
 
-export const obtenerCategoria = async () => {
-    const client = new Client({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PWD,
-        port: process.env.DB_PORT
-    })
+const app = express()
 
-    await client.connect()
-    console.log("Se realizó una conexión exitosa")
+app.use(express.json())
+app.use('/questions', questionRoutes)
 
-    const res = await client.query('SELECT * FROM question;')
-    const result = res.rows
-
-    await client.end()
-    return result
-}
-
-obtenerCategoria()
-    .then(result => console.log(result))
-    .catch(error => console.error("Error:", error))
+export default app
