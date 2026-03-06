9dd795ab4cc7eb81857421af47f7549a6ffb9637
jajodisant <pabonleonardo419@gmail.com>
2026-03-06 17:59:01 -0500

    prueba

--- src/app.js
+++ src/app.js
@@ -5,23 +5,32 @@ import questionRoutes from './modules/questions/question.route.js'
 import userRoutes from './modules/users/user.route.js'
 
 
-const express = require('express');
-const app = express()
+
+const app = express()   
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-
-
-
-app.use(express.static('index.html')); // tu carpeta HTML/JS
+const rootDir = path.join(__dirname, '..')
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(rootDir, 'index.html'))
+})
+
+//app.use(express.static('index.html')); // tu carpeta HTML/JS
+
+
+//app.use(express.static(path.join(__dirname, '../public')))
 app.use('/questions', questionRoutes)
 app.use('/users', userRoutes)
+
+
+const PORT = process.env.PORT || 3000
 
 // Resto de tu código de configuración y rutas
 app.listen(process.env.PORT || 3000, () => {
   console.log('App corriendo');
 });
+
 export default app
